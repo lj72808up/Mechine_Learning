120ca9b434324282bdc9009a91232d085fd3e113
liujie <liujie@zetyun.com>
2017-08-05 21:54:09 +0800

    Gaussian Bayes

--- Bayes/GaussianBayesTest.py
+++ Bayes/GaussianBayesTest.py
@@ -6,9 +6,9 @@ Y = np.array([1,1,1,2,2,2])
 
 from sklearn.naive_bayes import GaussianNB
 
-clf = GaussianNB()  #创建分类器
-clf.fit(X,Y)
-print clf.predict([[-0.8,-1]])
+clf = GaussianNB()   # 创建分类器
+clf.fit(X,Y)         # 使用训练特征和训练标签填充它
+print clf.predict([[-0.8,-1]])    # 使用分类器上的预测功能创建预测向量
 
 clf_pf = GaussianNB()
 clf_pf.partial_fit(X,Y,np.unique(Y))
---
+++ Bayes/GeographyGaussianNB/ClassifyNB.py
@@ -0,0 +1,6 @@
+def classify(features_train, labels_train):
+    #TODO your code goes here!
+    from sklearn.naive_bayes import GaussianNB
+    clf = GaussianNB()
+    clf.fit(features_train,labels_train)
+    return clf
